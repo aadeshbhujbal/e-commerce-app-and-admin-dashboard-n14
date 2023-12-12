3675e57da3a95f266a49b513f7206568c9bc65c2
Aadesh Santosh Bhujbal <aadeshbhujbal1@gmail.com>
2023-12-12 08:11:22 +0530

    footer_V2

--- src/app/_components/Footer/index.tsx
+++ src/app/_components/Footer/index.tsx
@@ -5,8 +5,6 @@ import { Footer } from '../../../payload/payload-types'
 import { fetchFooter } from '../../_api/fetchGlobals'
 import FooterComponent from './FooterComponent'
 
-import classes from './index.module.scss'
-
 export async function Footer() {
   let footer: Footer | null = null
 
